2b16aeb6522be091e3d7d035dafc1dccd09baf69
nicodiaz <ngdiaz00@gmailcom>
2024-12-12 11:03:12 -0300

    Se crea clase accesoDatos

---
+++ TPWinForm_equipo-J/negocio/AccesoDatos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database= CATALOGO_P3_DB; integrated security= true");
+            comando = new SqlCommand();
+        }
+    }
+}
