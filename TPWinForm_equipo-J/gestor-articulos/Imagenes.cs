3c2c4e628e926895b6f7446cf57ca246fbe7fd03
rubalva <rubenalfredoalvarado@gmail.com>
2024-12-11 21:36:47 -0600

    Atributos y Property Imagenes

--- TPWinForm_equipo-J/gestor-articulos/Imagenes.cs
+++ TPWinForm_equipo-J/gestor-articulos/Imagenes.cs
@@ -8,5 +8,10 @@ namespace gestor_articulos
 {
     internal class Imagenes
     {
+        public int Id { get; set; }
+        public string UrlImagen { get; set; }
+
+        // Asociacion
+        //public Articulos Articulos { get; set; }
     }
 }
